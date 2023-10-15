fca72c2c23592af356cffe8530bec47d49c65061
Daniel Bømark <daniel@Daniels-MBP.home>
2023-10-15 12:40:35 +0200

    Added comments in file 'data_tests.r' and switch name to correct df.

--- functions/data_tests.r
+++ functions/data_tests.r
@@ -6,11 +6,11 @@
 # all the aforementioned tests
 
 test_stations_metadata_colnames <-
-  function(df) {
-    
+  function(stations_metadata_df) {
+# Fill in expected col. names in the new df:
     expected_colnames <- c("id", "name", "latestData", "lat", "lon")
-    
-    if (all(colnames(df) == expected_colnames) == TRUE) {
+# Check if expected col. names corresponds to col. names in df
+    if (all(colnames(stations_metadata_df) == expected_colnames) == TRUE) {
       print("PASS: Data has the correct columns")
     } else{
       print("FAIL: Columns do not match the correct specification")
@@ -18,14 +18,14 @@ test_stations_metadata_colnames <-
   }
 
 test_stations_metadata_nrows <-
-  function(df) {
-    
+  function(stations_metadata_df) {
+# Add constraints for min and max rows in the df
     min_expected_rows <- 5000
     max_expected_rows <- 10000
-    
-    if (nrow(df) > min_expected_rows & nrow(df) < max_expected_rows) {
+# Check if the df fulfills the contraints and print result.
+    if (nrow(stations_metadata_df) > min_expected_rows & nrow(stations_metadata_df) < max_expected_rows) {
       print("PASS: Data has a reasonable number of rows")
-    } else if (nrow(df) <= min_expected_rows) {
+    } else if (nrow(stations_metadata_df) <= min_expected_rows) {
       print("FAIL: Data has suspiciously few rows")
     } else {
       print("FAIL: Data has suspiciously many rows")
@@ -33,11 +33,11 @@ test_stations_metadata_nrows <-
   }
 
 test_stations_metadata_coltypes <-
-  function(df) {
+  function(stations_metadata_df) {
     expected_coltypes <-
       c("character", "character", "double", "double", "double")
-    
-    if (all(df %>%
+# Fill in expected types for each column in df
+    if (all(stations_metadata_df %>%
             map_chr( ~ typeof(.)) == expected_coltypes) == TRUE) {
       print("PASS: All cols have the correct specifications")
     } else{
@@ -46,10 +46,10 @@ test_stations_metadata_coltypes <-
   }
   
 test_stations_metadata_nmissing <-
-  function(df) {
+  function(stations_metadata_df) {
     max_miss_vals <- 200
-    
-    if (df %>% map_int( ~ sum(is.na((.)))) %>% sum(.) < max_miss_vals) {
+# Constraint to check if the df has an acceptable amount of missing values
+    if (stations_metadata_df %>% map_int( ~ sum(is.na((.)))) %>% sum(.) < max_miss_vals) {
       print("PASS: Amount of missing values is reasonable")
     } else {
       print("FAIL: Too many missing values in data set")
@@ -57,9 +57,9 @@ test_stations_metadata_nmissing <-
   }
 
 test_stations_metadata_latestdata_timezone <-
-  function(df) {
-    
-    if (attr(df$latestData,"tzone")=="UTC") {
+  function(stations_metadata_df) {
+# Constraint to check if the df has the acceptable time zone or not
+    if (attr(stations_metadata_df$latestData,"tzone")=="UTC") {
       print("PASS: latestData has UTC-time zone")
     } else {
       print("FAIL: latestData does not have expected UTC-time zone")
@@ -68,12 +68,12 @@ test_stations_metadata_latestdata_timezone <-
 
 
 test_stations_metadata <- 
-  function(df){
-    test_stations_metadata_colnames(df)
-    test_stations_metadata_coltypes(df)
-    test_stations_metadata_nmissing(df)
-    test_stations_metadata_nrows(df)
-    test_stations_metadata_latestdata_timezone(df)
+  function(stations_metadata_df){
+    test_stations_metadata_colnames(stations_metadata_df)
+    test_stations_metadata_coltypes(stations_metadata_df)
+    test_stations_metadata_nmissing(stations_metadata_df)
+    test_stations_metadata_nrows(stations_metadata_df)
+    test_stations_metadata_latestdata_timezone(stations_metadata_df)
   }
 
 
